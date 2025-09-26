c3eed2ac90e1568c65ecfc7921d59e9147322485
ChauntelS <w0239497@campus.nscc.ca>
2025-09-26 14:34:07 -0300

    working on current weather screen

--- app/src/main/java/com/example/weatherapp/ui/screens/CurrentWeather.kt
+++ app/src/main/java/com/example/weatherapp/ui/screens/CurrentWeather.kt
@@ -1,2 +1,51 @@
 package com.example.weatherapp.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import com.example.weatherapp.R
+@Composable
+//Image
+fun CurrentWeather(){
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceAround,
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+        {
+            Image(painter = painterResource(id = R.drawable.part_cloudy),
+                contentDescription = "Partly Cloudy Icon*")
+        }
+        Row(
+            horizontalArrangement = Arrangement.SpaceAround,
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Text(text = "Partly Cloudy")
+        }
+            Text(
+                text = "14°C"
+            )
+            Text(
+                text = "Mostly Cloudy"
+            )
+            Text(
+                text = "Wind 19 km/h NE"
+            )
+        }
+    }}
